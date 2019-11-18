4664b6424f3d0eab4ddb4196d13e96343da9651d
nolec <ca10205@naver.com>
2019-11-18 17:37:41 +0900

    git git git

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,12 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Container = styled.div`
   &:not(:last-child) {
     margin-bottom: 50px;
   }
 `;
+const Title = styled.h3`
+  font-size: 14px;
+  font-weight: 600;
+`;
+const Grid = styled.div`
+  margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+  grid-gap: 25px;
+`;
+const Section = () => (
+  <Container>
+    <Title></Title>
+    <Grid></Grid>
+  </Container>
+);
 
-const Section = () => <Container>section에서 갑니다욘</Container>;
+Section.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Section;
